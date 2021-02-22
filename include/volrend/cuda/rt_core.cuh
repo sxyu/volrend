de12a27db133249fc86031d77db66d3fa7c0d563
Alex Yu <sxyu@berkeley.edu>
2021-02-22 09:57:18 -0800

    Support order 4 SH

--- include/volrend/cuda/rt_core.cuh
+++ include/volrend/cuda/rt_core.cuh
@@ -5,51 +5,79 @@
 namespace volrend {
 namespace device {
 namespace {
+// SH Coefficients from https://github.com/google/spherical-harmonics
+__device__ __constant__ const float C0 = 0.28209479177387814;
+__device__ __constant__ const float C1 = 0.4886025119029199;
+__device__ __constant__ const float C2[] = {
+    1.0925484305920792,
+    -1.0925484305920792,
+    0.31539156525252005,
+    -1.0925484305920792,
+    0.5462742152960396
+};
+
+__device__ __constant__ const float C3[] = {
+    -0.5900435899266435,
+    2.890611442640554,
+    -0.4570457994644658,
+    0.3731763325901154,
+    -0.4570457994644658,
+    1.445305721320277,
+    -0.5900435899266435
+};
+
+__device__ __constant__ const float C4[] = {
+    2.5033429417967046,
+    -1.7701307697799304,
+    0.9461746957575601,
+    -0.6690465435572892,
+    0.10578554691520431,
+    -0.6690465435572892,
+    0.47308734787878004,
+    -1.7701307697799304,
+    0.6258357354491761,
+};
 
 __device__ __inline__ void _precalc_sh(
     const int order,
     const float* __restrict__ dir,
     float* __restrict__ out_mult) {
-
-    const float C0 = 0.5 * sqrt(1.0 / M_PI);
-    const float C1 = sqrt(3 / (4 * M_PI));
-    const float C2[] = {0.5f * sqrtf(15.0f / M_PI),
-        -0.5f * sqrtf(15.0f / M_PI),
-        0.25f * sqrtf(5.0f / M_PI),
-        -0.5f * sqrtf(15.0f / M_PI),
-        0.25f * sqrtf(15.0f / M_PI)};
-
-    const float C3[] = {-0.25f * sqrtf(35 / (2 * M_PI)),
-        0.5f * sqrtf(105 / M_PI),
-        -0.25f * sqrtf(21/(2 * M_PI)),
-        0.25f * sqrtf(7 / M_PI),
-        -0.25f * sqrtf(21/(2 * M_PI)),
-        0.25f * sqrtf(105 / M_PI),
-        -0.25f * sqrtf(35/(2 * M_PI))
-    };
-
     out_mult[0] = C0;
     const float x = dir[0], y = dir[1], z = dir[2];
-    out_mult[1] = - C1 * y;
-    out_mult[2] = C1 * z;
-    out_mult[3] = -C1 * x;
-    if (order > 1) {
-        const float xx = x * x, yy = y * y, zz = z * z;
-        out_mult[4] = C2[0] * x * y;
-        out_mult[5] = C2[1] * y * z;
-        out_mult[6] = C2[2] * (2.0 * zz - xx - yy);
-        out_mult[7] = C2[3] * x * z;
-        out_mult[8] = C2[4] * (xx - yy);
-        if (order > 2) {
-            const float tmp_zzxxyy = 4 * zz - xx - yy;
+    const float xx = x * x, yy = y * y, zz = z * z;
+    const float xy = x * y, yz = y * z, xz = x * z;
+    switch (order) {
+        case 4:
+            out_mult[16] = C4[0] * xy * (xx - yy);
+            out_mult[17] = C4[1] * yz * (3 * xx - yy);
+            out_mult[18] = C4[2] * xy * (7 * zz - 1.f);
+            out_mult[19] = C4[3] * yz * (7 * zz - 3.f);
+            out_mult[20] = C4[4] * (zz * (35 * zz - 30) + 3);
+            out_mult[21] = C4[5] * xz * (7 * zz - 3);
+            out_mult[22] = C4[6] * (xx - yy) * (7 * zz - 1.f);
+            out_mult[23] = C4[7] * xz * (xx - 3 * yy);
+            out_mult[24] = C4[8] * (xx * (xx - 3 * yy) - yy * (3 * xx - yy));
+            [[fallthrough]];
+        case 3:
             out_mult[9] = C3[0] * y * (3 * xx - yy);
-            out_mult[10] = C3[1] * x * y * z;
-            out_mult[11] = C3[2] * y * tmp_zzxxyy;
+            out_mult[10] = C3[1] * xy * z;
+            out_mult[11] = C3[2] * y * (4 * zz - xx - yy);
             out_mult[12] = C3[3] * z * (2 * zz - 3 * xx - 3 * yy);
-            out_mult[13] = C3[4] * x * tmp_zzxxyy;
+            out_mult[13] = C3[4] * x * (4 * zz - xx - yy);
             out_mult[14] = C3[5] * z * (xx - yy);
             out_mult[15] = C3[6] * x * (xx - 3 * yy);
-        }
+            [[fallthrough]];
+        case 2:
+            out_mult[4] = C2[0] * xy;
+            out_mult[5] = C2[1] * yz;
+            out_mult[6] = C2[2] * (2.0 * zz - xx - yy);
+            out_mult[7] = C2[3] * xz;
+            out_mult[8] = C2[4] * (xx - yy);
+            [[fallthrough]];
+        case 1:
+            out_mult[1] = -C1 * y;
+            out_mult[2] = C1 * z;
+            out_mult[3] = -C1 * x;
     }
 }
 
@@ -108,7 +136,7 @@ __device__ __inline__ void trace_ray(
         out[0] = out[1] = out[2] = 0.0f;
         float pos[3], tmp;
         const float* tree_val;
-        float sh_mult[16];
+        float sh_mult[25];
         if (sh_order >= 0) {
             _precalc_sh(sh_order, vdir, sh_mult);
         }
